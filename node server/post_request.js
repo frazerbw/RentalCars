0853ed99caeb144604f95d4e7dd35269e519f5f6
Frazer <frazerde@gmail.com>
2017-11-10 00:12:34 +0000

    Finalized README and added ability to choose task as an arguments for the post_request.js script

--- node server/post_request.js
+++ node server/post_request.js
@@ -1,23 +1,40 @@
 var request = require('request');
 
+/*
+TASK LIST:
+1: Print a list of all the cars, in ascending price order
+2: Print a list of the specifications for each car
+3: Print out the highest rated supplier per car type, descending order
+4: Print out a list of vehicles, ordered by the sum of the scores in descending order
+*/
+
+// Getting task number desired from command line args
+var args = process.argv.slice(2);
+var taskChoice = args[0];
+
+if (taskChoice == undefined || taskChoice < 0 || taskChoice > 4) {
+  console.log("Invalid Task ID, please refer to README");
+  process.exit();
+}
+
 // Set the headers
 var headers = {
     'User-Agent':       'Super Agent/0.0.1',
     'Content-Type':     'application/x-www-form-urlencoded'
-}
+};
 
 // Configure the request
 var options = {
     url: 'http://127.0.0.1:8000',
     method: 'POST',
     headers: headers,
-    form: {'requestID': '2'}
-}
+    form: {'requestID': taskChoice}
+};
 
 // Start the request
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
-        console.log(body)
+        console.log(body);
     }
-})
+});
